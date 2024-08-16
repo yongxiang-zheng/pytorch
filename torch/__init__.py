10c4e8b1a87015b1199ac24300f0aa2d64023cb8
Yu, Guangye <guangye.yu@intel.com>
2024-08-16 16:21:45 +0000

    Add doc for device-agnostic runtime APIs
    ghstack-source-id: cbc8c0fa53c8d516e2b4f8ba7369a82969432719
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133323

--- torch/__init__.py
+++ torch/__init__.py
@@ -2590,6 +2590,24 @@ def get_device_module(device: _Optional[_Union[torch.device, str]] = None):
 
 
 class DeviceGuard:
+    r"""Context-manager that changes the selected device.
+
+    Args:
+        device_index (int): device index to select. It's a no-op if
+            this argument is negative.
+
+    Examples:
+        >>> # xdoctest: +SKIP("requires multiple accelerators.")
+        >>> torch.set_device(0)
+        >>> torch.current_device()
+        0
+        >>> with torch.DeviceGuard(1):
+        ...     torch.current_device()
+        1
+        >>> torch.current_device()
+        0
+    """
+
     def __init__(self, device_index: builtins.int):
         self.idx = device_index
         self.prev_idx = -1
@@ -2603,6 +2621,25 @@ class DeviceGuard:
 
 
 class StreamGuard:
+    r"""Context-manager that changes the selected stream.
+
+    Args:
+        stream (torch.Stream): selected stream.
+
+    Examples:
+        >>> # xdoctest: +REQUIRES(env:TORCH_DOCTEST_CUDA)
+        >>> s1 = torch.Stream()
+        >>> s2 = torch.Stream()
+        >>> torch.set_stream(s1)
+        >>> torch.current_stream() == s1
+        True
+        >>> with torch.StreamGuard(s2):
+        ...     torch.current_stream() == s2
+        True
+        >>> torch.current_stream() == s1
+        True
+    """
+
     def __init__(self, stream: torch.Stream):
         self.stream = stream
         self.src_prev_stream = None
