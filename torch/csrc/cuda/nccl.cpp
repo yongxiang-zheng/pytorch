a59b81d4f26d41275c40084da22657fb23643fe1
Dan Johnson <danielsjohnson@meta.com>
2024-08-26 14:46:59 -0700

    Use ncclAlltoAllv and ncclAlltoAll API when supported
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/134499
    
    NCCL does not have an api for ncclAllToAll and ncclAllToAllv, so PyTorch does point to point send/recv. Expose this API if it is supported.
    
    Differential Revision: [D61683836](https://our.internmc.facebook.com/intern/diff/D61683836/)
    ghstack-source-id: 239805272

--- torch/csrc/cuda/nccl.cpp
+++ torch/csrc/cuda/nccl.cpp
@@ -832,7 +832,7 @@ void all2all_single_equal_split(
   const auto* sendbuff = reinterpret_cast<const char*>(input.const_data_ptr());
   auto* recvbuff = reinterpret_cast<char*>(output.data_ptr());
   auto comm = to_nccl_comm(_comm);
-#if defined(USE_ROCM)
+#if defined(USE_ROCM) || defined(NCCL_ALLTOALL_SUPPORTED)
   NCCL_CHECK(ncclAllToAll(sendbuff, recvbuff, count, type, comm, stream));
 #else
   NCCL_CHECK(ncclCommCount(comm, &numranks));
@@ -877,6 +877,18 @@ void all2all_single_unequal_split(
 
   auto type = to_nccl_data_type(_type);
   auto comm = to_nccl_comm(_comm);
+#ifdef NCCL_ALLTOALLV_SUPPORTED
+  NCCL_CHECK(ncclAllToAllv(
+      sendbuff,
+      sendcounts,
+      senddispls,
+      recvbuff,
+      recvcounts,
+      recvdispls,
+      type,
+      comm,
+      stream.stream()));
+#else
   int numranks;
   NCCL_CHECK(ncclCommCount(comm, &numranks));
   NCCL_CHECK(ncclGroupStart());
@@ -905,6 +917,7 @@ void all2all_single_unequal_split(
 #else
   NCCL_CHECK_TIMEOUT(ncclGroupEnd(), _comm);
 #endif
+#endif
 #else
   AT_ERROR("all2all is only supported for NCCL lib version >= 2.7.0");
 #endif
@@ -924,6 +937,45 @@ void all2all(
   using namespace torch::cuda::nccl::detail;
   auto comm = to_nccl_comm(_comm);
 
+#ifdef NCCL_ALLTOALLV_SUPPORTED
+  TORCH_INTERNAL_ASSERT(
+      outputTensors.size() == inputTensors.size(),
+      "number of input tensors is not equal to number of output tensors");
+  std::vector<size_t> sendCounts(inputTensors.size());
+  std::vector<size_t> sendDisps(inputTensors.size());
+  std::vector<size_t> recvCounts(outputTensors.size());
+  std::vector<size_t> recvDisps(outputTensors.size());
+  uintptr_t sendBase = reinterpret_cast<uintptr_t>(inputTensors[0].data_ptr());
+  uintptr_t recvBase = reinterpret_cast<uintptr_t>(outputTensors[0].data_ptr());
+  size_t dtypeSize = inputTensors.front().element_size();
+
+  for (const auto r : c10::irange(outputTensors.size())) {
+    sendCounts[r] = inputTensors[r].numel();
+    auto sendOffset =
+        reinterpret_cast<uintptr_t>(inputTensors[r].data_ptr()) - sendBase;
+    TORCH_INTERNAL_ASSERT(
+        sendOffset % dtypeSize == 0,
+        "sendOffset is not divisible by dtypeSize");
+    sendDisps[r] = sendOffset / dtypeSize;
+    recvCounts[r] = outputTensors[r].numel();
+    auto recvOffset =
+        reinterpret_cast<uintptr_t>(outputTensors[r].data_ptr()) - recvBase;
+    TORCH_INTERNAL_ASSERT(
+        recvOffset % dtypeSize == 0,
+        "recvOffset is not divisible by dtypeSize");
+    recvDisps[r] = recvOffset / dtypeSize;
+  }
+  NCCL_CHECK(ncclAllToAllv(
+      inputTensors[0].data_ptr(),
+      sendCounts.data(),
+      sendDisps.data(),
+      outputTensors[0].data_ptr(),
+      recvCounts.data(),
+      recvDisps.data(),
+      to_nccl_data_type(inputTensors.front()),
+      comm,
+      stream.stream()));
+#else
   NCCL_CHECK(ncclGroupStart());
   for (const auto r : c10::irange(outputTensors.size())) {
     at::Tensor& input = inputTensors[r];
@@ -952,6 +1004,7 @@ void all2all(
   NCCL_CHECK(ncclGroupEnd());
 #else
   NCCL_CHECK_TIMEOUT(ncclGroupEnd(), _comm);
+#endif
 #endif
 #else
   AT_ERROR("all2all is only supported for NCCL lib version >= 2.7.0");
