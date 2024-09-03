ec4295c1c1e7add363fc9281ba33636c2bfadbc2
Will Feng <yf225@cornell.edu>
2024-09-02 23:27:58 -0700

    [Traceable FSDP2] Use .copy_ instead of .set_ for unsharded_param inplace update
    ghstack-source-id: e79af59ab4f6e9a3d2b5bcec35f816706a8c87bb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133730

--- torch/_inductor/comms.py
+++ torch/_inductor/comms.py
@@ -509,12 +509,11 @@ def enforce_comm_ordering_for_fsdp(
                 name_to_fused_node,
             )
 
-            # Find the "all_gather + all_gather_wait_tensor + copy_out + set_" code block
+            # Find the "all_gather + all_gather_wait_tensor + copy_out + copy_" code block
             allowed_ops = {
                 torch.ops._c10d_functional.all_gather_into_tensor_out.default,
                 torch.ops._c10d_functional.wait_tensor.default,
                 torch.ops.fsdp.split_with_sizes_copy.default,
-                torch.ops.aten.set_.source_Tensor,
             }
             find_recursive_users_of_node(
                 ag_snode,
@@ -526,6 +525,14 @@ def enforce_comm_ordering_for_fsdp(
                     or (
                         isinstance(x, scheduler.ExternKernelSchedulerNode)
                         and x.node.op_overload in allowed_ops  # type: ignore[union-attr]
+                    )
+                    or (
+                        isinstance(x, scheduler.SchedulerNode)
+                        and isinstance(x.node, ir.ComputedBuffer)
+                        and isinstance(x.node.data, ir.Pointwise)
+                        and len(x.node.data.origins) == 1
+                        and next(iter(x.node.data.origins)).target
+                        is torch.ops.fsdp.copy_.default
                     )
                 ),
             )
@@ -560,7 +567,7 @@ def enforce_comm_ordering_for_fsdp(
             assert wait_node_idx is not None
             ag_group_node = _create_group_node(ag_related_snodes[:wait_node_idx])
 
-            # Group "all_gather_wait_tensor + copy_out + set_" into one GroupedSchedulerNode
+            # Group "all_gather_wait_tensor + copy_out + copy_" into one GroupedSchedulerNode
             ag_wait_group_node = _create_group_node(ag_related_snodes[wait_node_idx:])
 
             ag_grouped_node_to_wait_grouped_node[ag_group_node] = ag_wait_group_node
